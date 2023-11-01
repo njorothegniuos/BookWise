57e9198e25a7454851d9d67b82dec3156d1a2266
Samwel Mburu <SamwelM@hollywoodbets.net>
2023-11-01 19:49:50 +0300

    Add BookConfiguration

---
+++ BookWise.Infrastructure/Configurations/BookConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BookWise.Domain.Entities.BookModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookWise.Infrastructure.Configurations
+{
+    internal class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("Books");
+
+            builder.HasKey(product => product.Id);
+            builder.Property(product => product.Title).HasMaxLength(150).IsRequired();
+            builder.Property(product => product.Author).HasMaxLength(150).IsRequired();
+            builder.Property(product => product.ISBN).HasMaxLength(150).IsRequired();
+
+        }
+    }
+}
