bc992ce1aac03637ef0d35b0e792cbce3d653884
Samwel Mburu <SamwelM@hollywoodbets.net>
2023-11-01 19:53:35 +0300

    Add BookOrderConfiguration

--- BookWise.Domain/Entities/BookModule/BookOrder.cs
+++ BookWise.Domain/Entities/BookModule/BookOrder.cs
@@ -5,8 +5,8 @@ namespace BookWise.Domain.Entities.BookModule
 {
     public class BookOrder : Entity
     {
-        public BookOrder(Guid id, Guid bookId, string userId, string reserveDate, 
-            string borrowedDate,string returnDate,RecordStatus recordStatus) : base(id)
+        public BookOrder(Guid id, Guid bookId, string userId, DateTime? reserveDate,
+            DateTime? borrowedDate, DateTime? returnDate,RecordStatus recordStatus) : base(id)
         {
             Id = id;
             BookId = bookId;
@@ -20,9 +20,9 @@ namespace BookWise.Domain.Entities.BookModule
         public Guid Id { get; set; }
         public Guid BookId { get; set; }
         public string UserId { get; set; }
-        public string ReserveDate { get; set; }
-        public string BorrowedDate { get; set; }
-        public string ReturnDate { get; set; }
+        public DateTime? ReserveDate { get; set; }
+        public DateTime? BorrowedDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
         public RecordStatus RecordStatus { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
---
+++ BookWise.Infrastructure/Configurations/BookOrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BookWise.Domain.Entities.BookModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookWise.Infrastructure.Configurations
+{
+    internal class BookOrderConfiguration : IEntityTypeConfiguration<BookOrder>
+    {
+        public void Configure(EntityTypeBuilder<BookOrder> builder)
+        {
+            builder.ToTable("BookOrders");
+
+            builder.HasKey(product => product.Id);
+            builder.Property(product => product.UserId).IsRequired();
+            builder.Property(product => product.BookId).IsRequired();
+        }
+    }
+}
